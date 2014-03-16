6820f662e4a4cb90fe7d986a569645afdc0ed8f9
Kate Crosby <kate.crosby@gmail.com>
2014-03-16 10:00:22 -0300

    Dryad

--- rdryad.R
+++ rdryad.R
@@ -6,6 +6,7 @@ require(rdryad)
 
 xml_file<-download_dryadmetadata(52360, transform=T, url = "http://www.datadryad.org/oai")
 
+###LONG
 getalldryad_metadata(transform=T, progress = "text", write=T, dir = T)
 
 search_dryad(xml_file, terms = 'CHONe', fuzzy=T)
